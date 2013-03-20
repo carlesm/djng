92369d6af574aa2e018f956354e0568322f8a8fa
Carles Mateu <carlesm@carlesm.com>
2013-03-20 12:19:24 +0100

    added model

--- sobres/isobres/views.py
+++ sobres/isobres/views.py
@@ -1,1 +1,16 @@
 # Create your views here.
+from django.http import HttpResponse
+
+def mainpage(request): 
+    output = '''
+        <html>
+        <head><title>%s</title></head>
+        <body>
+        <h1>%s</h1><p>%s</p>
+        </body>
+        </html>
+        ''' % ( 'Sobres aPP',
+        'Welcome to the Sobres aPPlication',
+        'Managing non legal funding since 2013')
+    return HttpResponse(output)
+
--- sobres/sobres/urls.py
+++ sobres/sobres/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import patterns, include, url
 
+
+from isobres.views import *
+
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'sobres.views.home', name='home'),
+    url(r'^$',mainpage, name='home'),
     # url(r'^sobres/', include('sobres.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
